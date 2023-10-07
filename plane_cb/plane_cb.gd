e093ce7b797fd3bbd2e31c54efc136ef2d8149bf
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-07 12:10:30 -0400

    Added player physics, laser, pipe and pipes prefabs.

---
+++ plane_cb/plane_cb.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const GRAVITY : float = 1900.0
+const POWER : float = -400.0
+
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	fly()
+	move_and_slide()
+	
+	if is_on_floor():
+		die()
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly"):
+		animation_player.stop()
+		velocity.y = POWER
+		animation_player.play("fly")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
